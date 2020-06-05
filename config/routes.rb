5fbf4098fc07ad89e1fb0f3e2f2ae05abdeb8cb2
EC2 Default User <ec2-user@ip-172-31-0-217.ap-northeast-1.compute.internal>
2020-06-05 09:45:22 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,21 @@ Rails.application.routes.draw do
   root 'projects#index'
   
   resources :projects, only: [:index, :new, :create, :destroy, :show] do
-      resources :tasks, only: [:new, :create, :show, :destroy, :edit, :update]
+    resources :tasks, only: [:new, :create, :show, :destroy, :edit, :update]
   end
+  
+  resources :tasks, only: [:new, :create, :show, :destroy, :edit, :update]
+  
+  # get 'tasks/new'
+  # get 'tasks/cereate'
+  # get 'tasks/show'
+  # get 'tasks/destroy'
+  # get 'tasks/edit'
+  # get 'tasks/update'
+  # get 'projects/index'
+  # get 'projects/new'
+  # get 'projects/create'
+  # get 'projects/destroy'
+  # get 'projects/show'
+  
 end
