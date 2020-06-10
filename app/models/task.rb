cff26d34a8c5d5cf43290cc8038a670df88dfbaf
EC2 Default User <ec2-user@ip-172-31-8-118.ap-northeast-1.compute.internal>
2020-06-10 11:06:07 +0000

    defined date and time in task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :project, dependent: :destroy
+  
+  def date
+    self.deadline.strftime("%Y年%m月%d日%a")
+  end
+  
+  def time
+    self.deadline_time.strftime("%H:%M")
+  end
+  
 end
